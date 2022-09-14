60063f1c3dedc1632e487b9639f15abc270ca63f
Anoop B <16367697+anoop-b@users.noreply.github.com>
2022-09-14 16:25:55 +0530

    fix: extend colors instead of replace

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    colors: {
-      "bg-black": "#171717",
-      "iff-orange": "#CC7755",
-      "body-grey": "#888888",
-      "bg-dark-grey": "#212121",
-      "bg-light-grey": "#2E2E2E",
-      "heading-black": "#171717",
-      "number-grey": "#E7E7E7",
+    extend: {
+      colors: {
+        "bg-black": "#171717",
+        "iff-orange": "#CC7755",
+        "body-grey": "#888888",
+        "bg-dark-grey": "#212121",
+        "bg-light-grey": "#2E2E2E",
+        "heading-black": "#171717",
+        "number-grey": "#E7E7E7",
+      },
     },
-    extend: {},
   },
   plugins: [
     require('@tailwindcss/typography'),
