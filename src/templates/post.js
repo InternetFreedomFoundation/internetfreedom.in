535f938740faffa9b6e409c492490d5d106aefd0
Anoop B <16367697+anoop-b@users.noreply.github.com>
2022-09-14 16:25:28 +0530

    feat: post layout

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,6 +1,6 @@
 import * as React from "react";
 import PropTypes from "prop-types";
-import { graphql } from "gatsby";
+import { graphql, Link } from "gatsby";
 import { Helmet } from "react-helmet";
 
 import { Layout } from "../components/common";
@@ -22,23 +22,48 @@ const Post = ({ data, location }) => {
         <style type="text/css">{`${post.codeinjection_styles}`}</style>
       </Helmet>
       <Layout>
-        <div className="container">
-          <article className="content">
-            {post.feature_image ? (
-              <figure className="post-feature-image">
-                <img src={post.feature_image} alt={post.title} />
-              </figure>
-            ) : null}
-            <section className="post-full-content">
-              <h1 className="content-title">{post.title}</h1>
+        <div className="px-4 py-4 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+          <p className="mx-auto text-center prose prose-sm">
+            {post.published_at_pretty}
+          </p>
+          <div className="md:mx-auto text-center lg:max-w-2xl">
 
-              {/* The main post content */}
-              <section
-                className="content-body load-external-scripts"
-                dangerouslySetInnerHTML={{ __html: post.html }}
+            <div
+              className="m-4 block text-4xl text-center font-extrabold text-gray-900"
+            >
+              {post.title}
+
+            </div>
+            <p className="mx-auto prose prose-slate">{post.excerpt}</p>
+          </div>
+          <div className="p-4 flex justify-center flex-col items-center">
+            <Link
+              to={`/author/${post.primary_author.slug}`}
+              aria-label="Author" className="inline-block mb-1">
+              <img
+                alt="avatar"
+                src={post.authors[0].profile_image}
+                className="object-cover w-10 h-10 rounded-full shadow-sm"
               />
-            </section>
-          </article>
+            </Link>
+            <div className="text-center">
+              <Link
+                to={`/author/${post.primary_author.slug}`}
+                aria-label="Author"
+                className="font-semibold text-gray-800"
+              >
+                {post.authors[0].name}
+              </Link>
+              <p className="text-sm font-medium leading-4 text-gray-400">
+                Primary Author
+              </p>
+            </div>
+          </div>
+          <hr className="my-8" />
+          <article
+            className="mt-4 prose prose-img:rounded-xl prose-img:shadow-xl mx-auto"
+            dangerouslySetInnerHTML={{ __html: post.html }}
+          ></article>
         </div>
       </Layout>
     </>
