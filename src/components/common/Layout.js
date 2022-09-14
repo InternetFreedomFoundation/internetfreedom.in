7a9269253b982b0759198e3c4736e3c03668f7fc
Anoop B <16367697+anoop-b@users.noreply.github.com>
2022-09-14 12:32:59 +0530

    feat: add footer, cleanup layout

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -5,10 +5,11 @@ import { Link, StaticQuery, graphql } from "gatsby";
 import { GatsbyImage } from "gatsby-plugin-image";
 
 import { Navigation } from ".";
+import { Footer } from ".";
 import config from "../../utils/siteConfig";
 
 // Styles
-import "../../styles/app.css";
+import "../../styles/global.css";
 
 /**
  * Main layout component
@@ -35,129 +36,13 @@ const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
         <body className={bodyClass} />
       </Helmet>
 
-      <div className="viewport">
-        <div className="viewport-top">
-          {/* The main header section on top of the screen */}
-          <header
-            className="site-head"
-            style={{
-              ...(site.cover_image && {
-                backgroundImage: `url(${site.cover_image})`,
-              }),
-            }}
-          >
-            <div className="container">
-              <div className="site-mast">
-                <div className="site-mast-left">
-                  <Link to="/">
-                    {site.logo ? (
-                      <img
-                        className="site-logo"
-                        src={site.logo}
-                        alt={site.title}
-                      />
-                    ) : (
-                      <GatsbyImage
-                        image={data.file.childImageSharp.gatsbyImageData}
-                        alt={site.title}
-                      />
-                    )}
-                  </Link>
-                </div>
-                <div className="site-mast-right">
-                  {site.twitter && (
-                    <a
-                      href={twitterUrl}
-                      className="site-nav-item"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <img
-                        className="site-nav-icon"
-                        src="/images/icons/twitter.svg"
-                        alt="Twitter"
-                      />
-                    </a>
-                  )}
-                  {site.facebook && (
-                    <a
-                      href={facebookUrl}
-                      className="site-nav-item"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <img
-                        className="site-nav-icon"
-                        src="/images/icons/facebook.svg"
-                        alt="Facebook"
-                      />
-                    </a>
-                  )}
-                  <a
-                    className="site-nav-item"
-                    href={`https://feedly.com/i/subscription/feed/${config.siteUrl}/rss/`}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <img
-                      className="site-nav-icon"
-                      src="/images/icons/rss.svg"
-                      alt="RSS Feed"
-                    />
-                  </a>
-                </div>
-              </div>
-              {isHome ? (
-                <div className="site-banner">
-                  <h1 className="site-banner-title">{site.title}</h1>
-                  <p className="site-banner-desc">{site.description}</p>
-                </div>
-              ) : null}
-              <nav className="site-nav">
-                <div className="site-nav-left">
-                  {/* The navigation items as setup in Ghost */}
-                  <Navigation data={site.navigation} navClass="site-nav-item" />
-                </div>
-                <div className="site-nav-right">
-                  <Link className="site-nav-button" to="/about">
-                    About
-                  </Link>
-                </div>
-              </nav>
-            </div>
-          </header>
+      <main>
+        {/* All the main content gets inserted here, index.js, post.js */}
+        {children}
+      </main>
 
-          <main className="site-main">
-            {/* All the main content gets inserted here, index.js, post.js */}
-            {children}
-          </main>
-        </div>
+      <Footer />
 
-        <div className="viewport-bottom">
-          {/* The footer at the very bottom of the screen */}
-          <footer className="site-foot">
-            <div className="site-foot-nav container">
-              <div className="site-foot-nav-left">
-                <Link to="/">{site.title}</Link> © 2021 &mdash; Published with{" "}
-                <a
-                  className="site-foot-nav-item"
-                  href="https://ghost.org"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Ghost
-                </a>
-              </div>
-              <div className="site-foot-nav-right">
-                <Navigation
-                  data={site.navigation}
-                  navClass="site-foot-nav-item"
-                />
-              </div>
-            </div>
-          </footer>
-        </div>
-      </div>
     </>
   );
 };
