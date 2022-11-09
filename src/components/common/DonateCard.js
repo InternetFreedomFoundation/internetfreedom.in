484a8f8b98d070ba40755157d6ab79e84b3212a7
Raghav Toshniwal <raghav.toshniwal@gmail.com>
2022-11-10 01:50:12 +0530

    css-fixes on donate widget

--- src/components/common/DonateCard.js
+++ src/components/common/DonateCard.js
@@ -15,16 +15,16 @@ const DonateCard = () => {
             <button class="border border-iff-orange hover:text-white text-iff-orange font-normal text-xl leading-6 not-italic py-2 rounded w-36 h-14">Know more</button>
           </div>
           <div id="donate-widget" className="ml-72 bg-bg-dark-grey w-[530px] h-[438px]">
-            <div class="text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700">
-                <ul class="flex flex-wrap -mb-px">
-                    <li class="mr-2">
-                        <a href="#" className="inline-block p-4 rounded-t-lg border-b-2 border-transparent hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300">Profile</a>
+            <div class="text-sm font-medium text-center text-gray-500">
+                <ul class="flex flex-wrap -mb-px bg-bg-light-grey">
+                    <li class="ml-16 mr-2">
+                        <a href="#" className="inline-block p-4 rounded-t-lg border-b-2 border-transparent hover:text-body-grey hover:border-gray-300">One time</a>
                     </li>
                     <li className="mr-2">
-                        <a href="#" className="inline-block p-4 text-blue-600 rounded-t-lg border-b-2 border-blue-600 active dark:text-blue-500 dark:border-blue-500" aria-current="page">Dashboard</a>
+                        <a href="#" className="inline-block p-4 text-iff-orange rounded-t-lg border-b-2 border-iff-orange active dark:iff-orange" aria-current="page">Monthly</a>
                     </li>
                     <li className="mr-2">
-                        <a href="#" className="inline-block p-4 rounded-t-lg border-b-2 border-transparent hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300">Settings</a>
+                        <a href="#" className="inline-block p-4 rounded-t-lg border-b-2 border-transparent hover:text-body-grey hover:border-gray-300">Annual</a>
                     </li>
                    
                 </ul>
