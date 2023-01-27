7d21b32648d3258362f50cf05b0ebf860ff7b1c6
Ameen Azeez <ameen4455@Ameens-MacBook-Pro.local>
2023-01-27 09:16:19 +0530

    Added navbar

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -7,6 +7,7 @@ import { GatsbyImage } from "gatsby-plugin-image";
 import { Navigation } from ".";
 import { Footer } from ".";
 import config from "../../utils/siteConfig";
+import { useState } from "react";
 
 // Styles
 //import "../../styles/app.css";
@@ -20,6 +21,7 @@ import config from "../../utils/siteConfig";
  *
  */
 const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
+  const [isOpen, setIsOpen] = useState(false);
   const site = data.allGhostSettings.edges[0].node;
   const localData = data.site.siteMetadata;
   const twitterUrl = site.twitter
@@ -37,49 +39,89 @@ const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
         <body className={bodyClass} />
       </Helmet>
 
+      {isOpen && (
+        <div className="w-full h-full lg:hidden bg-bg-light-grey fixed top-0 z-50">
+          <img
+            src="/images/icons/close.png"
+            onClick={() => setIsOpen(false)}
+            className="h-10 mt-4 ml-4 cursor-pointer"
+          />
+          <nav id="site-nav" className="flex flex-col mt-8 items-center">
+            {/* The navigation items as setup in Ghost */}
+            <Navigation
+              data={localData.navigation}
+              navClass="inline-block py-7 px-8 text-body-grey hover:opacity-100 hover:no-underline"
+            />
+          </nav>
+        </div>
+      )}
+
       <div className="flex flex-col justify-between">
         <div className="viewport-top">
           {/* The main header section on top of the screen */}
-          <header
-            className="bg-center bg-cover py-5 text-white bg-bg-black"
-            
-          >
-            <div id="container" className="my-0 mx-auto px-3.5">
+          <header className="bg-center bg-cover py-5 text-white bg-bg-black">
+            <div className="md:mx-auto max-w-screen-xl px-4">
               <div className="flex items-center justify-between" id="site-mast">
-                <div id="site-mast-left">
-                  <Link to="/">
-                    {isHome ? (
-                      null
-                    ) : (
-                      <img
-                        id="site-logo"
-                        className="h-12 ml-5 md:ml-48 "
-                        src="/images/logo.svg"
-                        alt={site.title}
-                      />
-                    )}
-                  </Link>
+                <Link to="/">
+                  {isHome ? null : (
+                    <img
+                      id="site-logo"
+                      className="h-12"
+                      src="/images/logo.svg"
+                      alt={site.title}
+                    />
+                  )}
+                </Link>
+
+                <div className="lg:hidden">
+                  <img
+                    onClick={() => setIsOpen(true)}
+                    src="/images/icons/menu.png"
+                    className="h-10 cursor-pointer"
+                  />
                 </div>
-                <div id="site-mast-right" className="flex items-center last:pr-0">
-                  <nav id="site-nav" className="flex items-center justify-between mx-0 mb-0 mt-4 pr-48">
-                    <div className="site-nav-left">
-                      {/* The navigation items as setup in Ghost */}
-                      <Navigation data={localData.navigation} navClass="inline-block py-1 px-8 text-body-grey hover:opacity-100 hover:no-underline" />
-                    </div>
+                <div
+                  id="site-mast-right"
+                  className="lg:flex items-center hidden"
+                >
+                  <nav id="site-nav" className="flex items-center">
+                    {/* The navigation items as setup in Ghost */}
+                    <Navigation
+                      data={localData.navigation}
+                      navClass="inline-block py-1 px-8 text-body-grey hover:opacity-100 hover:no-underline"
+                    />
                   </nav>
                 </div>
               </div>
               {isHome ? (
-                <div id="site-banner" className="my-0 ml-5 md:ml-48 mx-auto">
-                  <img src="/images/logo.svg" className="h-28 mt-28"/>
-                  <h1 id=" site-banner-title" className="mt-12 font-bold text-2xl leading-7 not-italic max-w-lg">{localData.description}</h1>
-                  <p id="site-banner-desc" className="my-4 max-w-lg text-body-grey text-xl leading-6 not-italic">{localData.subDescription}</p>
-                  <p id="site-banner-desc" className="my-4 max-w-lg text-body-grey text-xl leading-6 not-italic mb-14">{localData.cta}</p>
+                <div id="site-banner" className="my-0 mx-auto">
+                  <img src="/images/logo.svg" className="h-28 mt-28" />
+                  <h1
+                    id=" site-banner-title"
+                    className="mt-12 font-bold text-2xl leading-7 not-italic max-w-lg"
+                  >
+                    {localData.description}
+                  </h1>
+                  <p
+                    id="site-banner-desc"
+                    className="my-4 max-w-lg text-body-grey text-xl leading-6 not-italic"
+                  >
+                    {localData.subDescription}
+                  </p>
+                  <p
+                    id="site-banner-desc"
+                    className="my-4 max-w-lg text-body-grey text-xl leading-6 not-italic mb-14"
+                  >
+                    {localData.cta}
+                  </p>
                   <div id="action" className="mb-40">
-                    <button class="bg-iff-orange hover:bg-iff-orange-700 text-white font-normal text-xl leading-6 not-italic py-2 px-4 rounded w-36 h-14">Donate</button>
-                    <button class="border border-iff-orange hover:text-white text-iff-orange font-normal text-xl leading-6 not-italic py-2 ml-8 rounded w-36 h-14">Subscribe</button>
+                    <button class="bg-iff-orange hover:bg-iff-orange-700 text-white font-normal text-xl leading-6 not-italic py-2 px-4 rounded w-36 h-14">
+                      Donate
+                    </button>
+                    <button class="border border-iff-orange hover:text-white text-iff-orange font-normal text-xl leading-6 not-italic py-2 ml-8 rounded w-36 h-14">
+                      Subscribe
+                    </button>
                   </div>
-
                 </div>
               ) : null}
             </div>
@@ -87,12 +129,10 @@ const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
 
           <main id="site-main">
             {/* All the main content gets inserted here, index.js, post.js */}
-            {children} 
+            {children}
           </main>
         </div>
         <Footer />
-
-        
       </div>
     </>
   );
@@ -120,7 +160,7 @@ const DefaultLayoutSettingsQuery = (props) => (
           }
         }
         site {
-          ...LocalSettingsFields                                                          
+          ...LocalSettingsFields
         }
         file(relativePath: { eq: "ghost-icon.png" }) {
           childImageSharp {
@@ -134,3 +174,4 @@ const DefaultLayoutSettingsQuery = (props) => (
 );
 
 export default DefaultLayoutSettingsQuery;
+
--- src/components/common/Navigation.js
+++ src/components/common/Navigation.js
@@ -35,7 +35,7 @@ const Navigation = ({ data, navClass }) => (
         );
       }
     })}
-    <div className={navClass}>
+    {/* <div className={navClass}>
       <div class="relative flex items-center h-8 rounded-full focus-within:shadow-lg bg-bg-dark-grey overflow-hidden">
           <div class="grid place-items-center h-full w-12 text-gray-300">
               <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -49,7 +49,7 @@ const Navigation = ({ data, navClass }) => (
           id="search"
           placeholder="Search" /> 
       </div>
-    </div>
+    </div> */}
   </>
 );
 
