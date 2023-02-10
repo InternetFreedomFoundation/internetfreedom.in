24db827d25c05a93e909fc3a2f88fce3ac087ae5
Anoop B <16367697+anoop-b@users.noreply.github.com>
2023-02-10 18:15:08 +0200

    chore: update site metadata

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -7,13 +7,12 @@ module.exports = {
 
   postsPerPage: 8, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
-  siteTitleMeta: `Ghost Gatsby Starter`, // This allows an alternative site title for meta data for pages.
-  siteDescriptionMeta: `A starter template to build amazing static websites with Ghost and Gatsby`, // This allows an alternative site description for meta data for pages.
-
+  siteTitleMeta: `Internet Freedom Foundation`, // This allows an alternative site title for meta data for pages.
+  siteDescriptionMeta: `Internet Freedom Foundation defends online freedom, privacy and innovation in India`,
   shareImageWidth: 1000, // Change to the width of your default share image
   shareImageHeight: 523, // Change to the height of your default share image
 
-  shortTitle: `Ghost`, // Used for App manifest e.g. Mobile Home Screen
+  shortTitle: `IFF`, // Used for App manifest e.g. Mobile Home Screen
   siteIcon: `favicon.png`, // Logo in /static dir used for SEO, RSS, and App manifest
   backgroundColor: `#e9e9e9`, // Used for Offline Manifest
   themeColor: `#15171A`, // Used for Offline Manifest
