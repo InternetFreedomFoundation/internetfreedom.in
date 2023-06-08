24906d2b2761b7c2bd49e06233ceb22f54fa3f8b
Anoop B <16367697+anoop-b@users.noreply.github.com>
2023-06-08 20:29:18 +0300

    feat: SEO component for custom pages

---
+++ src/components/seo.jsx
@@ -0,0 +1,30 @@
+import React from "react"
+
+export const SEO = ({ title, description, url, children }) => {
+
+    const seo = {
+        title: title,
+        description: description,
+        url: url,
+        image: "https://internetfreedom.in/images/blog_placeholder_image.png",
+    }
+
+    return (
+        <>
+            <title>{seo.title}</title>
+            <meta name="description" content={seo.description} />
+            <meta name="image" content={seo.image} />
+            <meta property="og:url" content={seo.url} />
+            <meta property="og:type" content="website" />
+            <meta property="og:title" content={seo.title} />
+            <meta property="og:description" content={seo.description} />
+            <meta property="og:image" content={seo.image} />
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta name="twitter:title" content={seo.title} />
+            <meta name="twitter:url" content={seo.url} />
+            <meta name="twitter:description" content={seo.description} />
+            <meta name="twitter:image" content={seo.image} />
+            {children}
+        </>
+    )
+}
