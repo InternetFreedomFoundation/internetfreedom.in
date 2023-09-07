fa55e4cb8fab8859fc27b2bcaa04335c813f0d56
Anoop B <16367697+anoop-b@users.noreply.github.com>
2023-09-07 22:51:34 +0200

    refactor: update copy

--- src/pages/letter-to-trai-to-protect-net-neutrality-again.js
+++ src/pages/letter-to-trai-to-protect-net-neutrality-again.js
@@ -44,9 +44,8 @@ function Content() {
     return (
 
         <div className="px-4 py-6 md:py-16 prose md:prose-xl hover:prose-headings:underline underline-offset-4 hover:prose-a:text-iff-orange responsive-container">
-
-            <h1>2 steps to protect Net Neutrality:</h1>
-            <h2>Step 1: Write to TRAI</h2>
+            <h1>A few clicks to #SaveTheInternet</h1>
+            <h2>Write to TRAI</h2>
             <p>
                 <strong>Click the button below and hit send:</strong> When you click on the button below, an email compose box will open on your device. Relevant details, including the body of the email (counter-comments), recipients (to: Advisor, TRAI; cc: IFF), and subject line, will already be filled out. If you wish to add your name and affiliation at the bottom of the email body, please do so. Or you can send the email as it is.
             </p>
@@ -63,23 +62,6 @@ function Content() {
             <p>
                 [If the above method doesn’t work for you, please find the relevant details for sending the email, including the draft text for counter-comments <a href="https://docs.google.com/document/d/1VUBpje6_dVceqkHkMir2vBZfE6hDafYEVVQd7jcgi44/edit?usp=sharing">here</a>]
             </p>
-
-            <h2>Step 2: Register for the OHD</h2>
-
-            <p>
-                Our mission doesn’t end here. TRAI is organising a virtual Open House Discussion (OHD) on the consultation paper on September 19, 2023. Register for the OHD today and make sure your voice is heard. This will potentially be the last chance under this consultation process for us internet freedom fighters to save the internet.
-            </p>
-            <div className="text-center">
-                <button
-                    type="button"
-                    className="btn-secondary"
-                    onClick={() => {
-                        window.open('https://www.airmeet.com/e/23e95f10-4c9c-11ee-9132-bb6a19360c2e', '_blank');
-                    }}
-                >
-                    Register for the OHD by TRAI
-                </button>
-            </div>
 
             <p>
                 The recommendation given by TSPs would imply imposing service licences on everything OTT — communication apps like WhatsApp or Signal at the least, payment apps such as PayTM, broadcasting platforms like Netflix or Mubi, and a range of other services — for them to be able to operate in India. The telcos are in agreement that OTT services, apps and websites may be selectively banned at source and without implicating the underlying telecom service provider in the process. At the outset, the consultation paper  fails to capture the vast and vibrant world of internet-based apps and services by condensing it to “OTT”, and is further misguided in suggesting regulation and licences. In 2016, TRAI took measures to safeguard online service and content providers from differential pricing and    unfair competition, which continues to ensure an open, free and neutral internet.  But now, TRAI’s policy objective seems to align with telcos’ interests. Submissions made by telcos further confirm that imposing licences on OTTs  will only profit a chosen few. Us users, on the other hand, only stand to lose. Bringing OTT apps under licences and imposing high operation costs upon them may drive smaller players out of the market, limiting user choice and preference, or leading to a spike in usage or subscription fees. And that fundamentally threatens net neutrality. What’s more, data-secure apps like Signal may find themselves on thin ice with authorities who may require them to break end-to-end encryption or implement customer verification. In summary: this policy move may jeopardise user privacy and autonomy, restrict innovation, and concentrate power in the hands of a few.
