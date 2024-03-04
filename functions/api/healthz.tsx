52f2d93e4b1880ccda9ccc3746e7c16a504b7e98
Anoop B <16367697+anoop-b@users.noreply.github.com>
2024-03-04 21:05:09 +0530

    refactor: moved metadata object

--- functions/api/healthz.tsx
+++ functions/api/healthz.tsx
@@ -1,12 +1,14 @@
 export const onRequestGet: PagesFunction = async (context) => {
     return new Response(JSON.stringify({
         "status": "ok",
-        "city": String(context.request.cf.city),
-        "isp": String(context.request.cf.asOrganization),
-        "country": String(context.request.cf.country),
-        "pincode": String(context.request.cf.postalCode),
-        "region": String(context.request.cf.region),
-        "ip": String(context.request.headers.get('CF-Connecting-IP')),
-        "ua": String(context.request.headers.get('user-agent')),
+        "metadata": {
+            "city": String(context.request.cf.city),
+            "ISP": String(context.request.cf.asOrganization),
+            "country": String(context.request.cf.country),
+            "pincode": String(context.request.cf.postalCode),
+            "region": String(context.request.cf.region),
+            "ip": String(context.request.headers.get('CF-Connecting-IP')),
+            "UA": String(context.request.headers.get('user-agent')),
+        },
     }));
 };
