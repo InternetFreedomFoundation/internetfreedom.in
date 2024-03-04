b2c1c43491105c6fb0b7ea0917d5d8c12e0b2404
Anoop B <16367697+anoop-b@users.noreply.github.com>
2024-03-04 20:46:59 +0530

    feat: added metadata to /healthz endpoint

--- functions/api/healthz.tsx
+++ functions/api/healthz.tsx
@@ -1,5 +1,12 @@
-export const onRequestGet: PagesFunction = () => {
+export const onRequestGet: PagesFunction = async (context) => {
     return new Response(JSON.stringify({
-        "status": "ok"
+        "status": "ok",
+        "city": String(context.request.cf.city),
+        "isp": String(context.request.cf.asOrganization),
+        "country": String(context.request.cf.country),
+        "pincode": String(context.request.cf.postalCode),
+        "region": String(context.request.cf.region),
+        "ip": String(context.request.headers.get('CF-Connecting-IP')),
+        "ua": String(context.request.headers.get('user-agent')),
     }));
 };
