fd78c942bcd4eae0fb9a452d11b5326dfd686421
Anoop B <16367697+anoop-b@users.noreply.github.com>
2024-04-17 23:35:56 +0300

    fix: confimation screen perks

--- src/components/DonateWidget.js
+++ src/components/DonateWidget.js
@@ -520,7 +520,32 @@ const Confirmation = ({
             ₹{currentMembership.amount}
           </span>
         </div>
-        <span className="text-[#888888]">{currentMembership.description}</span>
+        <div className="">
+            <p className="font-bold text-white">
+              {currentMembership.title} membership perks
+            </p>
+            <ul>
+              {currentMembership.perks.map((perk) => (
+                  <ul className="my-4 text-left text-gray-400">
+                    <li className="flex items-center space-x-3">
+                      <svg
+                        className="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                          clip-rule="evenodd"
+                        ></path>
+                      </svg>
+                      <span>{perk}</span>
+                    </li>
+                  </ul>
+                ))}
+            </ul>
+          </div> 
       </div>
       <div className="bg-[#2E2E2E] w-full mt-10 sm:mt-0 sm:w-1/4 rounded-md">
         <div className="w-full h-full p-8 flex flex-col items-center justify-center space-y-5">
