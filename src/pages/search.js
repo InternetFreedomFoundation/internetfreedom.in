0f23f5b40d01b13a31751f8f9f01272fd65050f0
Anoop B <16367697+anoop-b@users.noreply.github.com>
2024-06-25 16:48:44 +0300

    feat: add search disclosure

--- src/pages/search.js
+++ src/pages/search.js
@@ -12,8 +12,13 @@ export default function NotFoundPage() {
                             <h1 className="mt-2 text-4xl font-bold text-center text-gray-900 tracking-tight sm:text-5xl">
                                 Search the Site
                             </h1>
-                           <div className="p-4">
-                            <Search/>
+                            <div className="p-4">
+                                <div >
+                                    <p className="prose mx-auto text-center">
+                                        We do not and cannot track what you type, Search to your hearts content!
+                                    </p>
+                                </div>
+                                <Search />
                             </div>
                         </div>
 
@@ -26,9 +31,9 @@ export default function NotFoundPage() {
 
 export function Head() {
     return (
-      <div>
-        <link href="/pagefind/pagefind-ui.css" rel="stylesheet" />
-        <script src="/pagefind/pagefind-ui.js"></script>
-      </div>
+        <div>
+            <link href="/pagefind/pagefind-ui.css" rel="stylesheet" />
+            <script src="/pagefind/pagefind-ui.js"></script>
+        </div>
     )
-  }
+}
