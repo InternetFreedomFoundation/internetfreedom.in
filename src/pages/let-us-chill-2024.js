4d3d13cd42c3bb03967f102c3e48150240ca34d9
Anoop B <16367697+anoop-b@users.noreply.github.com>
2024-08-01 12:56:27 +0300

    fix: links and formatting

--- src/pages/let-us-chill-2024.js
+++ src/pages/let-us-chill-2024.js
@@ -35,8 +35,8 @@ function TopSection() {
                 <div className="text-gray-300">
                     <p>
                         Hey, remember the Broadcasting Bill, 2023 - it has made a comeback except it is now worse! MIB has been (secretly) revising the Bill - what has been consistent is that the Bill will target your favourite online curated (Netflix and Hotstar) shows and your most reliable content creators, including those sharing news(!) on online platforms (YouTube, Instagram, & such others). The Bill will clamp down on our right to free expression—giving the Union government powers to censor content they find unpalatable. The revised, 2024 version of the Bill (which MIB has shared with only a few industry members) will impose a Code of Conduct similar to one applicable to Cable TV broadcasters on any news content creators who monetise their content as well as creators who use skill/expertise (Ambiguity Alert).
-                        <br/>
-                        <br/>
+                        <br />
+                        <br />
                         It is time AGAIN to act! Join us, push back against this bill. Tell the MIB: #KillTheBill & #LetUsChill
                     </p>
                 </div>
@@ -52,7 +52,7 @@ function Content() {
         <div className="px-4 py-6 md:py-16 prose md:prose-xl hover:prose-headings:underline underline-offset-4 hover:prose-a:text-iff-orange responsive-container">
             <h2>A quick update from 2024: Plot twist, but the rights-infringing kind</h2>
             <p>
-                If you think the Broadcasting Bill, 2023 shared for public consultation is bad, wait till you hear what the Ministry has been up to. The MIB has conducted 4(!!) <a href="https://content.internetfreedom.in/api/files/divco3ywedt9rpe/pmm8vyid1gq2mos/iff_2024_052_letter_requesting_public_consultation_for_revised_broadcasting_bill_12_07_6w8mCPP6S8.2024.pdf">closed door meetings</a> (that has been reported upon so far) with selective industry representatives, without representation from civil society, journalists, or other key stakeholders. Further, each shared copy of the revised bill bears a watermark across every page that is unique to the stakeholder so that the MIB can trace leaks. Stakeholders were also asked to give an undertaking that they would not share the Bill further (a minute please… we’re left speechless).
+                If you think the Broadcasting Bill, 2023 shared for public consultation is bad, wait till you hear what the Ministry has been up to. The MIB has conducted 4(!!) <a href="https://www.hindustantimes.com/india-news/confusion-persists-over-digital-news-content-in-proposed-broadcasting-bill-101720725258677.html">closed door meetings</a> (that has been reported upon so far) with selective industry representatives, without representation from civil society, journalists, or other key stakeholders. Further, each shared copy of the revised bill bears a watermark across every page that is unique to the stakeholder so that the MIB can trace leaks. Stakeholders were also asked to give an undertaking that they would not share the Bill further (a minute please… we’re left speechless).
             </p>
             <h3>Consultations cannot be exclusive!</h3>
 
@@ -91,7 +91,7 @@ function Content() {
                             <li>Instagram: @mib_india</li>
                             <li>Refer to the template tweets/graphics below:
                                 <p>
-                                     <em>Folder: <a href="https://drive.google.com/drive/folders/1MzLBtR8ibCdVAvQuqdCrV4hyHGEBB-FA">Broadcasting Bill | Social media storm</a></em>
+                                    <em>Folder: <a href="https://drive.google.com/drive/folders/1MzLBtR8ibCdVAvQuqdCrV4hyHGEBB-FA">Broadcasting Bill | Social media storm</a></em>
                                 </p>
                             </li>
                         </ul>
@@ -146,29 +146,29 @@ function Content() {
                 <li><strong>Letters to MIB</strong>   </li>
                 <ol>
                     <li>In February 2024, we <a
-                        href="https://drive.google.com/file/d/1UBiA0S2uzKbtAU5BGTyEqxJPhCxiA1kF/view?usp=sharing">wrote</a>
+                        href="https://drive.google.com/file/d/1UBiA0S2uzKbtAU5BGTyEqxJPhCxiA1kF/view?usp=sharing">wrote </a>
                         to MIB requesting them to make all responses received during the public
                         consultation process on the draft 2023 Bill publicly available on their website
                         (with personal information of responders redacted), and allow stakeholders to
                         send counter comments.</li>
                     <li>In March 2024, we <a
-                        href="https://drive.google.com/file/d/18Bp0K_eC5Z_2_fCz7t4ca_CVIW0Vanrg/view?usp=sharing">engaged</a>
+                        href="https://drive.google.com/file/d/18Bp0K_eC5Z_2_fCz7t4ca_CVIW0Vanrg/view?usp=sharing">engaged </a>
                         with the Standing Committee on Communications and Information Technology (IT) on
                         their 56th report ‘Regulation of Cable Television in India’ relating to action
                         taken by MIB, expressing our opposition to the Bill.   </li>
                     <li>In April 2024, upon being asked by MIB if our comments on the Bill can be
                         made public under the Right to Information (“RTI”) Act, we <a
-                            href="https://drive.google.com/file/d/1J25QPGV07B_7TgQQjBo4ZtFwo0lrPVNr/view?usp=sharing">provided</a>
+                            href="https://drive.google.com/file/d/1J25QPGV07B_7TgQQjBo4ZtFwo0lrPVNr/view?usp=sharing">provided </a>
                         our consent.   </li>
                     <li>Simultaneously, we <a
-                        href="https://drive.google.com/file/d/1gZ8xaLCgo81vteW8jFdVNLAUn2Zu9CJz/view?usp=sharing">wrote</a>
+                        href="https://drive.google.com/file/d/1gZ8xaLCgo81vteW8jFdVNLAUn2Zu9CJz/view?usp=sharing">wrote </a>
                         to MIB noting that while seeking consent of stakeholders before sharing their
                         response under the RTI Act is appreciated, best practice dictates that MIB must
                         proactively publish comments publicly on their website.   </li>
                     <li>In July 2024, noting the circulation of a new revised draft Bill, we <a
-                        href="https://drive.google.com/file/d/1SaVT-3eOGY3wsykWhmvK5VxXVXcIf91L/view?usp=sharing">wrote</a>
+                        href="https://drive.google.com/file/d/1SaVT-3eOGY3wsykWhmvK5VxXVXcIf91L/view?usp=sharing">wrote </a>
                         to MIB urging them to make this version publicly available and open it for
-                        meaningful public consultation.   </li>
+                        meaningful public consultation.</li>
                 </ol>
                 <li>Alongside advocacy and engagement, we have been relying on the RTI Act to
                     glean information about stakeholders and their comments to the Bill. In February
@@ -188,7 +188,7 @@ function Content() {
                 <li>We have been engaging with Members of Parliament and urging them to express
                     their opposition against the Bill in all its forms. To this effect, we have
                     compiled a <a
-                        href="https://docs.google.com/document/d/1v6FNRXNm_DBUasL2I76gko396bD1wqZm7tHcihdNLTg/edit">comprehensive
+                        href="https://drive.google.com/file/d/1H1jM_b8Cljxt-JgjDnADDkKdjeRXrBq0/view?usp=sharing">comprehensive
                         explainer</a> for MPs decoding the harms of the Bill and the threats such a
                     regime will pose to Constitutional free speech.</li>
             </ol>
@@ -206,21 +206,21 @@ function Content() {
                         href="https://content.internetfreedom.in/api/files/divco3ywedt9rpe/j1qbgrb1z4092pd/iff_s_comments_on_broadcasting_bill_2023_gTIK6GVbjP.pdf?ref=static.internetfreedom.in">link</a>)
                     </li>
                     <li>IFF’s letter to MIB requesting publication of comments received (<a
-                        href="https://drive.google.com/file/d/1UBiA0S2uzKbtAU5BGTyEqxJPhCxiA1kF/view">link</a>
+                        href="https://drive.google.com/file/d/1UBiA0S2uzKbtAU5BGTyEqxJPhCxiA1kF/view">link </a>
                         and <a
                             href="https://drive.google.com/file/d/1gZ8xaLCgo81vteW8jFdVNLAUn2Zu9CJz/view?usp=sharing">link</a>)
                     </li>
                     <li>Article about closed-door meetings with industry stakeholders (<a
-                        href="https://www.hindustantimes.com/india-news/confusion-persists-over-digital-news-content-in-proposed-broadcasting-bill-101720725258677.html">link</a>
+                        href="https://www.hindustantimes.com/india-news/confusion-persists-over-digital-news-content-in-proposed-broadcasting-bill-101720725258677.html">link </a>
                         and <a
-                            href="https://www.medianama.com/2024/07/223-india-broadcast-bill-online-creators/">link</a>)
+                            href="https://www.hindustantimes.com/india-news/new-draft-of-broadcasting-bill-news-influencers-may-be-classified-as-broadcasters-101721961764666.html">link</a>)
                     </li>
                     <li>IFF’s letter to MIB requesting public consultation on the revised
                         Broadcasting Bill, 2023 (<a
                             href="https://drive.google.com/file/d/1SaVT-3eOGY3wsykWhmvK5VxXVXcIf91L/view?usp=sharing">link</a>)
                     </li>
                     <li>Article about the various revisions in the Broadcasting Bill, 2023 (<a
-                        href="https://drive.google.com/file/d/1aFL0TdnXhtmseEQIGGz9ISlUxUgznrS0/view?usp=drive_link">link</a>)
+                        href="https://www.medianama.com/2024/07/223-india-broadcast-bill-online-creators/">link</a>)
                     </li>
                     <li>IFF’s post citing concerns regarding reported revisions and opacity in the
                         consultation process (<a
@@ -238,7 +238,7 @@ function Content() {
                         href="https://drive.google.com/file/d/1P15dYFaXbSBEE2GTpCMmEa8gXZmz04Eu/view?usp=sharing">link</a>)
                     </li>
                     <li>Open letter to MIB signed by creators (<a
-                        href="https://drive.google.com/file/d/1fYOCmS2p7S0dTUIernfpJ-kHWv74NHf7/view">link</a>)
+                        href="https://docs.google.com/forms/d/e/1FAIpQLSecW_bDTbiSHe9upQZbNJnzTsVX8Ab7T_SZL532ccGR2ce6OQ/viewform?usp=send_form">link</a>)
                     </li>
                 </ol>
             </div>
